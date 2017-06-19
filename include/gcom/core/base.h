7b6b7f6f77bcac1e597476e21ca596029a7e5f16
gcomdev <gcomdev@gmail.com>
2017-06-19 23:59:25 +0300

    Refactored base
    Pure virtual functions are implemented just in case.
    Some documentation is added.

--- include/gcom/core/base.h
+++ include/gcom/core/base.h
@@ -1,99 +1,73 @@
 #ifndef GCOM_BASE_H
 #define GCOM_BASE_H
 
+#include <gcom/core/defs.h>
 #include <gcom/core/meta.h>
-#include <gcom/core/globals.h>
 #include <gcom/utils/shared.h>
+#include <cstdio>
+#include <memory>
+#include <string>
 #include <vector>
 
 namespace gcom
 {
+	/// \brief Base class for all classes that have a **name**, a **state**, and can hold children objects. All classes inheriting gcom::base has the method start(), which first starts all the children, and then starts itself.
+	///
+	/// Library users should not inherit from this class. Instead, they should inherit gcom::link, gcom::computer, or gcom::graph.
+	/// Library developers may inherit this class to create other classes, whose objects could be held by other gcom::base derived objects.
+	///
+	/// When start() is called;
+	/// 1. start() method of the children objects are called one by one. If all returns **GCOM_OK**, execution continues. If any returns some value other than **GCOM_OK**, stop() method is called and the first encountered error is returned.
+	/// 2. inter_start() method is called. inter_start() method may be overriden by the library developers, not by users. This method is intended for internal initializations. If inter_start() returns **GCOM_OK**, execution continue. Else, stop() method is called and the encountered error is returned.
+	/// 3. on_start() method is called. on_start() method may be overriden by the library users, not by developers. If on_start() returns **GCOM_OK**, execution continue. Else, stop() method is called and the encountered error is returned.
+	/// 4. **state** attribute is set accordingly.
+	///
+	/// When stop() is called, almost the reverse operation is done. Only difference is, inter_stop() is called before on_stop().
+	///
+	/// **name** need not be unique. **parent** attribute is handled by add_child() and remove_children() methods automaticly.
 	class base : public meta
 	{
 	protected:
-		base()
-		{
-			set_id(g_base_id++);
-			set_type(GCOM_BASE);
-			set_state(GCOM_STOPPED);
-			set_parent(nullptr);
-		}
+		base();
+
+	public:
+		int start();
+		void stop();
+	protected:
+		virtual int inter_start();
+		virtual void inter_stop();
+	private:
+		virtual int on_start();
+		virtual void on_stop();
 
 	private:
 		std::string m_name;
 	public:
-		std::string get_name() { return m_name; }
-	public:
-		void set_name(std::string &name) { m_name = name; }
+		std::string get_name();
+		void set_name(std::string&);
 
 	private:
 		shared<int> m_state;
 	public:
-		int get_state() { return m_state.get(); }
-	public:
-		void wait_state(int state) { m_state.wait(state); }
+		int get_state();
+		void wait_state(int);
 	protected:
-		void set_state(int state) { m_state.set(state); }
+		void set_state(int);
 
 	private:
 		std::vector< std::shared_ptr<base> > m_children;
 	public:
-		base* get_child(size_t no) { return no < m_children.size() ? m_children[no].get() : nullptr; }
-		size_t get_children_count() { return m_children.size(); }
-	public:
-		void add_child(std::shared_ptr<base> obj) { if (obj) { obj->set_parent(this); m_children.push_back(obj); } }
-		void remove_children() { m_children.clear(); }
+		base* get_child(size_t);
+		size_t get_children_count();
+		void add_child(std::shared_ptr<base>);
+		void remove_children();
 
 	private:
 		base *m_parent;
 	public:
-		base* get_parent() { return m_parent; }
+		base* get_parent();
 	private:
-		void set_parent(base *parent) { m_parent = parent; }
-
-	public:
-		int start()
-		{
-			if (get_state() == GCOM_STARTED)
-				return GCOM_OK;
-
-			int result;
-			for (size_t i = 0; i < get_children_count(); i++)
-			{
-				result = get_child(i)->start();
-				if (result != GCOM_OK)
-				{
-					stop();
-					return result;
-				}
-			}
-			if ((result = on_start()) != GCOM_OK)
-				return result;
-			if ((result = inter_start()) != GCOM_OK)
-			{
-				on_stop();
-				return result;
-			}
-			set_state(GCOM_STARTED);
-			return GCOM_OK;
-		}
-		void stop()
-		{
-			if (get_state() == GCOM_STOPPED)
-				return;
-
-			set_state(GCOM_STOPPED);
-			inter_stop();
-			on_stop();
-			for (size_t i = 0; i < get_children_count(); i++)
-				get_child(i)->stop();
-		}
-	protected:
-		virtual int inter_start() = 0;
-		virtual void inter_stop() = 0;
-	private:
-		virtual int on_start() = 0;
-		virtual void on_stop() = 0;
+		void set_parent(base*);
 	};
 }
 
--- src/base.cc
+++ src/base.cc
@@ -1,1 +1,83 @@
 #include <gcom/core/base.h>
+#include <gcom/core/globals.h>
+
+gcom::base::base()
+{
+	set_id(gcom::g_base_id++);
+	set_type(GCOM_BASE);
+	set_state(GCOM_STOPPED);
+	set_parent(nullptr);
+}
+
+int gcom::base::start()
+{
+	if (get_state() == GCOM_STARTED)
+		return GCOM_OK;
+
+	int result;
+	for (size_t i = 0; i < get_children_count(); i++)
+	{
+		result = get_child(i)->start();
+		if (result != GCOM_OK)
+		{
+			stop();
+			return result;
+		}
+	}
+	if ((result = on_start()) != GCOM_OK)
+		return result;
+	if ((result = inter_start()) != GCOM_OK)
+	{
+		on_stop();
+		return result;
+	}
+	set_state(GCOM_STARTED);
+	return GCOM_OK;
+}
+
+void gcom::base::stop()
+{
+	if (get_state() == GCOM_STOPPED)
+		return;
+
+	set_state(GCOM_STOPPED);
+	inter_stop();
+	on_stop();
+	for (size_t i = 0; i < get_children_count(); i++)
+		get_child(i)->stop();
+}
+
+int gcom::base::inter_start() { return GCOM_OK; }
+
+void gcom::base::inter_stop() {}
+
+int gcom::base::on_start() { return GCOM_OK; }
+
+void gcom::base::on_stop() {}
+
+std::string gcom::base::get_name() { return m_name; }
+
+int gcom::base::get_state() { return m_state.get(); }
+
+void gcom::base::wait_state(int state) { m_state.wait(state); }
+
+gcom::base* gcom::base::get_child(size_t no) { return no < m_children.size() ? m_children[no].get() : nullptr; }
+
+size_t gcom::base::get_children_count() { return m_children.size(); }
+
+gcom::base* gcom::base::get_parent() { return m_parent; }
+
+void gcom::base::set_name(std::string &name) { m_name = name; }
+
+void gcom::base::add_child(std::shared_ptr<base> obj) { if (obj) { obj->set_parent(this); m_children.push_back(obj); } }
+
+void gcom::base::remove_children()
+{
+	for (size_t i = 0; i < get_children_count(); i++)
+		get_child(i)->set_parent(nullptr);
+	m_children.clear();
+}
+
+void gcom::base::set_state(int state) { m_state.set(state); }
+
+void gcom::base::set_parent(gcom::base *parent) { m_parent = parent; }
